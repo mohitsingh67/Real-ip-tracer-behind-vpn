eb35ba1bb637257aab5771489040d0c69bbedb43
Mohit Singh Gurjar <117108809+mohitsingh67@users.noreply.github.com>
2025-02-09 16:21:46 +0530

    Update trip.php

--- modules/trip.php
+++ modules/trip.php
@@ -1,62 +1,55 @@
 <?php
-function trac() {
-  logo();
-  $cyn = "\033[01;36m";
-  $yel = "\033[01;33m";
-  $prompt = "\n\033[00m";
-  echo $prompt;
-  
-  $ip = readline('  Enter IP : ');
-  $data = @unserialize(file_get_contents('http://ip-api.com/php/' . $ip));
-
-  $FCL = "\033[01;33m";
-  $MCL = "\033[01;37m>\033[01;32m";
-  $NCL = "\033[00m";
-  
+function tracm() {
+  $data = @unserialize(file_get_contents('http://ip-api.com/php/'));
+  $FCL="\033[01;33m";
+  $MCL="\033[01;37m>\033[01;32m";
+  $NCL="\033[00m";
+  system(PHP_OS_FAMILY === 'Windows' ? "cls" : "clear");
   date_default_timezone_set($data['timezone']);
-  system("clear");
-  
   echo <<<EOL
 \033[01;33m
 
-\033[01;31m      _\033[01;33m _____     _        _     _      
-     \033[01;31m(_)\033[01;33m |_   _|   (_)      | |   (_)     
 
-   \033[01;37m}\033[01;31m------------------------------------------------\033[01;37m{  
-}\033[01;31m------ \033[01;32mReal IP Tracking Behind VPN\033[01;31m ------\033[01;37m{  
-}\033[01;31m------ \033[01;32mDeveloped by Team - TejasVigil\033[01;31m ------\033[01;37m{  
-}\033[01;31m---- \033[01;32mMembers: Mohit Singh, Naveen Kumar, Ravindra Rajak\033[01;31m ----\033[01;37m{  
-   }\033[01;31m------------------------------------------------\033[01;37m{  
+\033[01;31m  _______         _     
+ \033[01;31m|__   __|       | |    
+ \033[01;33m   | | ___  __ _| |__  
+ \033[01;33m   | |/ _ \/ _` | '_ \ 
+ \033[01;32m   | |  __/ (_| | | | |
+ \033[01;32m   |_|\___|\__,_|_| |_|
+ \033[01;36m   TEAM TEJAS | IP TRACKER
+
+   \033[01;37m}\033[01;31m----------------------------------------\033[01;37m{
+}\033[01;31m--------------- \033[01;32mIP Information\033[01;31m ---------------\033[01;37m{
+   }\033[01;31m----------------------------------------\033[01;37m{
 
 \033[00m
 EOL;
 
-  if ($data['status'] == 'success') {
-    echo "\n " . $FCL . "IP Address    " . $MCL . "   " . $data['query'];
-    echo "\n " . $FCL . "Country code  " . $MCL . "   " . $data['countryCode'];
-    echo "\n " . $FCL . "Country       " . $MCL . "   " . $data['country'];
-    echo "\n " . $FCL . "Date & Time   " . $MCL . "   " . date("F j, Y, g:i a");
-    echo "\n " . $FCL . "Region code   " . $MCL . "   " . $data['region'];
-    echo "\n " . $FCL . "Region        " . $MCL . "   " . $data['regionName'];
-    echo "\n " . $FCL . "City          " . $MCL . "   " . $data['city'];
-    echo "\n " . $FCL . "Zip code      " . $MCL . "   " . $data['zip'];
-    echo "\n " . $FCL . "Time zone     " . $MCL . "   " . $data['timezone'];
-    echo "\n " . $FCL . "ISP           " . $MCL . "   " . $data['isp'];
-    echo "\n " . $FCL . "Organization  " . $MCL . "   " . $data['org'];
-    echo "\n " . $FCL . "ASN           " . $MCL . "   " . $data['as'];
-    echo "\n " . $FCL . "Latitude      " . $MCL . "   " . $data['lat'];
-    echo "\n " . $FCL . "Longitude     " . $MCL . "   " . $data['lon'];
-    echo "\n " . $FCL . "Location      " . $MCL . "   " . $data['lat'] . "," . $data['lon'];
+  if($data['status'] == 'success') {
+    echo "\n ".$FCL."IP Address    ".$MCL."   ".$data['query'];
+    echo "\n ".$FCL."Country code  ".$MCL."   ".$data['countryCode'];
+    echo "\n ".$FCL."Country       ".$MCL."   ".$data['country'];
+    echo "\n ".$FCL."Date & Time   ".$MCL."   ".date("F j, Y, g:i a");
+    echo "\n ".$FCL."Region code   ".$MCL."   ".$data['region'];
+    echo "\n ".$FCL."Region        ".$MCL."   ".$data['regionName'];
+    echo "\n ".$FCL."City          ".$MCL."   ".$data['city'];
+    echo "\n ".$FCL."Zip code      ".$MCL."   ".$data['zip'];
+    echo "\n ".$FCL."Time zone     ".$MCL."   ".$data['timezone'];
+    echo "\n ".$FCL."ISP           ".$MCL."   ".$data['isp'];
+    echo "\n ".$FCL."Organization  ".$MCL."   ".$data['org'];
+    echo "\n ".$FCL."ASN           ".$MCL."   ".$data['as'];
+    echo "\n ".$FCL."Latitude      ".$MCL."   ".$data['lat'];
+    echo "\n ".$FCL."Longitude     ".$MCL."   ".$data['lon'];
+    echo "\n ".$FCL."Location      ".$MCL."   ".$data['lat'].",".$data['lon'];
     echo "\n\n$NCL";
   } else {
     echo "\n\033[01;31m Sorry, unable to track your\033[01;33m IP Address\033[01;31m !!!\033[00m\n";
     echo "\033[01;31m Check your \033[01;33mNetwork connection\033[01;31m !!\033[00m\n";
-    echo "\033[01;31m If you are \033[01;33mOnline\033[01;31m, then check your \033[01;33mIP Address\033[01;31m !!\033[00m\n\n";
+    echo "\033[01;31m If you are \033[01;33mOnline\033[01;31m then check your \033[01;33mIP Address\033[01;31m !!\033[00m\n\n";
   }
-
-  $prompt = "\033[00m";
+  $prompt="\033[01;37m";
   echo $prompt;
-  $getact = readline(' TejasVigil-IP-Tracer >> ');
+  $getact = readline(' TEJAS-TRACKER >> ');
   menu();
 }
 ?>
